94f28b86cc4b2bd91d22d58e039b83d5a5f2142c
pam <justin.parado.personal@gmail.com>
2023-07-04 22:52:26 +0800

    Fitler log data

--- src/app/logs/logs.component.ts
+++ src/app/logs/logs.component.ts
@@ -1,50 +1,45 @@
-import { Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
-import { AngularFireDatabase } from '@angular/fire/compat/database';
-import { HttpClient } from '@angular/common/http';
+import {Component, ElementRef, QueryList, ViewChildren} from "@angular/core";
+import {AngularFireDatabase} from "@angular/fire/compat/database";
+import {HttpClient} from "@angular/common/http";
+import {selectedLogModel} from "../_shared/models/selectedlog.model";
 
-import { map, take } from 'rxjs/operators';
-import { Observable, merge } from 'rxjs';
-import { selectedLogModel } from '../_shared/models/selectedlog.model';
 @Component({
-  selector: 'app-logs',
-  templateUrl: './logs.component.html',
-  styleUrls: ['./logs.component.scss']
+	selector: "app-logs",
+	templateUrl: "./logs.component.html",
+	styleUrls: ["./logs.component.scss"],
 })
-export class LogsComponent {
-  @ViewChildren('confirmModal, resultModal,exportModal') myModals!: QueryList<ElementRef>;
-  employees!: any[];
-  registeredUsers!: any[];
-  registeredUsers2!: any[];
-  sensorData!: any[];
-  sensorData2!: any[];
-  selectedUID!:string;
-  selectedLog: selectedLogModel = {
-    checker: '',
-    connected: 0,
-    flame: '',
-    flameStatus: '',
-    loc: '',
-    mq2: 0,
-    mq135Status: '',
-    status: '',
-    StatusNotif: '',
-    coordinates: '',
-    deviceId: '',
-    mq2status: '',
-    mq135: 0,
-    key: '',
-    location:''
-  }
+export class LogsComponent
+{
+	@ViewChildren("confirmModal, resultModal,exportModal") myModals!: QueryList<ElementRef>;
+	employees!: any[];
+	registeredUsers!: any[];
+	registeredUsers2!: any[];
+	sensorData!: any[];
+	sensorData2!: any[];
+	selectedUID!: string;
+	selectedLog: selectedLogModel = {
+		checker: "",
+		connected: 0,
+		flame: "",
+		flameStatus: "",
+		loc: "",
+		mq2: 0,
+		mq135Status: "",
+		status: "",
+		StatusNotif: "",
+		coordinates: "",
+		deviceId: "",
+		mq2status: "",
+		mq135: 0,
+		key: "",
+		location: "",
+	};
+	highlighted = true;
 
-  constructor(private afDatabase: AngularFireDatabase, private http: HttpClient) {
+	constructor(private afDatabase: AngularFireDatabase, private http: HttpClient)
+	{
 
-  }
-  ngOnInit(): void {
-    this.retrievelogs();
-    throw new Error('Method not implemented.');
-    
-  }
-
+	}
 
 
 //   retrievelogs() {
@@ -57,7 +52,7 @@ export class LogsComponent {
 // //       this.sensorData = sensorData;
 // //       console.log(sensorData, 'sensordata');
 // // this.getLocations(this.sensorData);
-      
+
 // //     });
 
 // merge(
@@ -72,137 +67,160 @@ export class LogsComponent {
 //   console.log(sensorData1,'sensordat1');
 // });
 
- //  }
+	//  }
 
- retrievelogs() {
-      this.afDatabase.list('/Logs').snapshotChanges().subscribe(sensorDataSnapshot => {
-        const sensorData = sensorDataSnapshot.map(dataSnapshot => {
-          const key = dataSnapshot.key;
-          const value = dataSnapshot.payload.val() as Record<string, any>;
-          return { key, ...value };
-        });
-        this.sensorData = sensorData;
-        console.log(sensorData, 'sensordata');
- // this.getLocations(this.sensorData);
-      });
- }
+	ngOnInit(): void
+	{
+		this.retrievelogs();
+	}
 
-  
+	retrievelogs()
+	{
+		this.afDatabase.list("/Logs").snapshotChanges().subscribe(sensorDataSnapshot =>
+		{
+			const sensorData = sensorDataSnapshot.map(dataSnapshot =>
+			{
+				const key = dataSnapshot.key;
+				const value = dataSnapshot.payload.val() as Record<string, any>;
+				return {key, ...value};
+			});
+
+			const filteredData = sensorData.map(data =>
+			{
+				return {
+					// @ts-ignore
+					sensorDataValues: data.sensorDataValues,
+					// @ts-ignore
+					userData: data.userData,
+					// @ts-ignore
+					timestamp: data.timestamp,
+				};
+			});
+
+			this.sensorData = sensorData;
+			console.log({filteredData});
+			// this.getLocations(this.sensorData);
+		});
+	}
+
+	// openModalConfirm(data: any) {
+	//   console.log(data, 'selected data');
+	//   const selectedLog = data;
+
+	//   if (selectedLog) {
+	//     const [latitude, longitude] = selectedLog.loc.split(',');
+	//     const apiUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=YOUR_API_KEY`;
+
+	//     this.http.get<any>(apiUrl).subscribe(
+	//       (locationData) => {
+	//         if (locationData.status === 'OK' && locationData.results.length > 0) {
+	//           const locationString = locationData.results[0].formatted_address;
+	//           selectedLog.location = locationString;
+	//           this.selectedLog = selectedLog;
+	//           console.log(selectedLog.flame, 'OPENMODAL');
+	//           this.selectedUID = selectedLog.userId;
+	//           this.myModals.toArray()[0].nativeElement.classList.add('show');
+	//           this.myModals.toArray()[0].nativeElement.style.display = 'block';
+	//           document.body.classList.add('modal-open');
+	//         }
+	//       },
+	//       (error) => {
+	//         console.log('Error fetching location data:', error);
+	//       }
+	//     );
+	//   } else {
+	//     console.log('User not found', selectedLog);
+	//   }
+	// }
+
+	getLocations(users: any[]): void
+	{
+		users.forEach(user =>
+		{
+			const [latitude, longitude] = user.loc.split(",");
+			const apiUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyAKEou6JOvFfcgPt_G-W3cGXnGn-g8W82w`;
+
+			this.http.get<any>(apiUrl).subscribe(
+				(data) =>
+				{
+					if (data.status === "OK" && data.results.length > 0)
+					{
+						const locationString = data.results[0].formatted_address;
+						user.location = locationString;
+					}
+				},
+				(error) =>
+				{
+					console.log("Error fetching location data:", error);
+				},
+			);
+		});
+	}
+
+	openModalConfirm(data: any)
+	{
+		console.log(data, "selected data");
+		const selectedLog = data;
+		if (selectedLog)
+		{
+
+			this.selectedLog = selectedLog;
+
+			console.log(selectedLog.flame, "OPENMODAL");
+			this.selectedUID = selectedLog.userId;
+			this.myModals.toArray()[0].nativeElement.classList.add("show");
+			this.myModals.toArray()[0].nativeElement.style.display = "block";
+			document.body.classList.add("modal-open");
+		}
+		else
+		{
+			console.log("User not found", selectedLog);
+		}
+	}
+
+	closeModalConfirm()
+	{
+
+		//this.searchBooklet();
+		this.myModals.toArray()[0].nativeElement.classList.remove("show");
+		this.myModals.toArray()[0].nativeElement.style.display = "none";
+		document.body.classList.remove("modal-open");
+	}
 
 
-  
-  getLocations(users: any[]): void {
-    users.forEach(user => {
-      const [latitude, longitude] = user.loc.split(',');
-      const apiUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyAKEou6JOvFfcgPt_G-W3cGXnGn-g8W82w`;
-  
-      this.http.get<any>(apiUrl).subscribe(
-        (data) => {
-          if (data.status === 'OK' && data.results.length > 0) {
-            const locationString = data.results[0].formatted_address;
-            user.location = locationString;
-          }
-        },
-        (error) => {
-          console.log('Error fetching location data:', error);
-        }
-      );
-    });
-  }
-  
-  // openModalConfirm(data: any) {
-  //   console.log(data, 'selected data');
-  //   const selectedLog = data;
-  
-  //   if (selectedLog) {
-  //     const [latitude, longitude] = selectedLog.loc.split(',');
-  //     const apiUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=YOUR_API_KEY`;
-  
-  //     this.http.get<any>(apiUrl).subscribe(
-  //       (locationData) => {
-  //         if (locationData.status === 'OK' && locationData.results.length > 0) {
-  //           const locationString = locationData.results[0].formatted_address;
-  //           selectedLog.location = locationString;
-  //           this.selectedLog = selectedLog;
-  //           console.log(selectedLog.flame, 'OPENMODAL');
-  //           this.selectedUID = selectedLog.userId;
-  //           this.myModals.toArray()[0].nativeElement.classList.add('show');
-  //           this.myModals.toArray()[0].nativeElement.style.display = 'block';
-  //           document.body.classList.add('modal-open');
-  //         }
-  //       },
-  //       (error) => {
-  //         console.log('Error fetching location data:', error);
-  //       }
-  //     );
-  //   } else {
-  //     console.log('User not found', selectedLog);
-  //   }
-  // }
-  
+	// deleteUsersWithIdNode() {
+	//   this.afDatabase.list('/Registered Users').snapshotChanges().subscribe((users) => {
+	//     users.forEach((user) => {
+	//       const userId = user.key; // Get the user ID
 
-  openModalConfirm(data:any) {
-    console.log(data,'selected data')
-    const selectedLog =  data;
-    if (selectedLog) {
+	//       const userRef = this.afDatabase.object(`/Registered Users/${userId}`);
+	//       userRef.snapshotChanges().subscribe((snapshot) => {
+	//         if (snapshot.payload.exists() && snapshot.payload.child('id').exists()) {
+	//           userRef.remove(); // Remove the user with the 'id' node
+	//         }
+	//       });
+	//     });
+	//   });
+	// }
 
-    this.selectedLog = selectedLog;
-    
-    console.log(selectedLog.flame,'OPENMODAL');
-    this.selectedUID=selectedLog.userId;
-    this.myModals.toArray()[0].nativeElement.classList.add('show');
-    this.myModals.toArray()[0].nativeElement.style.display = 'block';
-    document.body.classList.add('modal-open');
-    } else {
-      console.log('User not found',selectedLog);
-    }
-  }
+	updateUser(hashid: string, newData: Partial<selectedLogModel>)
+	{
+		const userId = "Registered Users/User Id";
+		const userRef = this.afDatabase.object(`${userId}/${hashid}`);
 
+		userRef.update(newData)
+			.then(() =>
+			{
+				console.log("User updated successfully");
+			})
+			.catch((error: any) =>
+			{
+				console.error("Error updating user:", error);
+			});
+	}
 
-  closeModalConfirm() {
-
-    //this.searchBooklet();
-    this.myModals.toArray()[0].nativeElement.classList.remove('show');
-    this.myModals.toArray()[0].nativeElement.style.display = 'none';
-    document.body.classList.remove('modal-open');
-  }
-
-
-  
-
-  updateUser(hashid: string, newData: Partial<selectedLogModel>) {
-    const userId = 'Registered Users/User Id';
-    const userRef = this.afDatabase.object(`${userId}/${hashid}`);
-    
-    userRef.update(newData)
-      .then(() => {
-        console.log('User updated successfully');
-      })
-      .catch((error: any) => {
-        console.error('Error updating user:', error);
-      });
-  }
-  
-  
-    // deleteUsersWithIdNode() {
-  //   this.afDatabase.list('/Registered Users').snapshotChanges().subscribe((users) => {
-  //     users.forEach((user) => {
-  //       const userId = user.key; // Get the user ID
-        
-  //       const userRef = this.afDatabase.object(`/Registered Users/${userId}`);
-  //       userRef.snapshotChanges().subscribe((snapshot) => {
-  //         if (snapshot.payload.exists() && snapshot.payload.child('id').exists()) {
-  //           userRef.remove(); // Remove the user with the 'id' node
-  //         }
-  //       });
-  //     });
-  //   });
-  // }
-  
-  
-
-  highlighted = true;
-  toggleOpacity() {
-    this.highlighted = !this.highlighted;
-  }
+	toggleOpacity()
+	{
+		this.highlighted = !this.highlighted;
+	}
 }
