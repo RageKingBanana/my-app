2ff29ea0baded6a98f304e40336a44d5eedfc336
Ricky James B. Perez <36735566+RageKingBanana@users.noreply.github.com>
2023-07-04 18:15:59 +0800

    logs available

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 export class AppComponent {
   constructor(public afAuth: AngularFireAuth, private router: Router) {}
 
-  title = 'my-app';
+  title = 'PYRONNOIA';
 
   async logout() {
     try {
